9bf40c264c81f720f0e2e472971f1f3cf03bafc8
Spaceboy Ross <spaceboyross@yandex.com>
2018-12-08 23:26:32 -0800

    Fixed login

--- configs/osjs/airootfs/root/customize_airootfs.sh
+++ configs/osjs/airootfs/root/customize_airootfs.sh
@@ -12,7 +12,8 @@ cp -aT /etc/skel/ /root/
 chmod 700 /root
 
 groupadd osjs
-useradd osjs -p osjs -d /home/osjs -g osjs
+useradd osjs -p `openssl passwd -1 osjs` -d /home/osjs -g osjs
+useradd demo -p `openssl passwd -1 osjs` -d /home/osjs -g osjs
 
 sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
